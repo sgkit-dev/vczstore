9a41b8b535820caf2259e144fdc0a82dbe031cf3
Tom White <tom.e.white@gmail.com>
2026-05-11 11:06:06 +0100

    Simplify implementation

--- tests/test_rechunk.py
+++ tests/test_rechunk.py
@@ -1,15 +1,10 @@
-import pathlib
-
 from vcztools.utils import open_zarr
 
 from tests.utils import make_vcz
 from vczstore.rechunk import rechunk
-from vczstore.utils import copy_store_to_icechunk
 
 
-def test_rechunk(
-    tmpdir,
-):
+def test_rechunk():
     vcz = make_vcz(
         variant_contig=[0, 0, 0, 0],
         variant_position=[1, 2, 3, 4],
@@ -29,18 +24,10 @@ def test_rechunk(
         variants_chunk_size=2,
     )
 
-    ic_tmp_path = pathlib.Path(tmpdir) / "icechunk"
-    ic_tmp_path.mkdir()
-    vcz_ic = pathlib.Path(ic_tmp_path) / "store.vcz"
-    copy_store_to_icechunk(vcz, vcz_ic)
+    rechunk(vcz, "variant_contig", 4)
 
-    rechunk(vcz_ic, "variant_contig", 4, backend_storage="icechunk")
-
-    root = open_zarr(vcz_ic, backend_storage="icechunk")
+    root = open_zarr(vcz)
     assert root["variant_contig"].chunks[0] == 4
     assert root["variant_position"].chunks[0] == 2
     assert root["variant_allele"].chunks[0] == 2
     assert root["call_genotype"].chunks[0] == 2
-
-    # check deleted variant is not in root
-    assert "variant_contig_delete" not in root
--- vczstore/rechunk.py
+++ vczstore/rechunk.py
@@ -1,7 +1,7 @@
-from bio2zarr.zarr_utils import create_empty_group_array, get_compressor_config
+from bio2zarr.zarr_utils import create_group_array, get_compressor_config
 from vcztools.utils import array_dims, open_zarr
 
-from vczstore.utils import transaction, variant_chunk_slices
+from vczstore.utils import transaction
 
 
 def rechunk(
@@ -9,53 +9,34 @@ def rechunk(
     variants_array_name,
     variants_chunk_size,
     *,
-    backend_storage="icechunk",
+    backend_storage=None,
 ):
     """Rechunk a variants array with a larger variants chunk size that is
     an exact multiple of the existing chunk size."""
 
-    if backend_storage != "icechunk":
-        raise ValueError("Only icechunk storage is supported for rechunk")
-
     # TODO: check pre-conditions
 
-    var = variants_array_name
-    var_rechunked = f"{var}_rechunked"
-    var_delete = f"{var}_delete"
+    with transaction(vcz, backend_storage=backend_storage, message="rechunk") as vcz:
+        root = open_zarr(vcz, mode="r+", backend_storage=backend_storage)
 
-    # separate transactions for data and restructuring operations as they can't be mixed
-    # https://icechunk.io/en/stable/guides/moving-nodes/#rearrange-sessions
-    with transaction(vcz, backend_storage=backend_storage, message="rechunk") as store:
-        root = open_zarr(store, mode="r+", backend_storage=backend_storage)
-        variant_chunks_in_batch = variants_chunk_size // root[var].chunks[0]
-
+        var = variants_array_name
         arr = root[var]
-
         new_chunks = (variants_chunk_size,) + arr.chunks[1:]
 
-        create_empty_group_array(
+        # read entire array into memory
+        data = arr[:]
+
+        # delete the array
+        del root[var]
+
+        # recreate the array with new chunk size
+        create_group_array(
             root,
-            var_rechunked,
+            var,
+            data=data,
             shape=arr.shape,
             dtype=arr.dtype,
             chunks=new_chunks,
             compressor=get_compressor_config(arr),
             dimension_names=array_dims(arr),
         )
-        arr_rechunked = root[var_rechunked]
-
-        for v_sel in variant_chunk_slices(root, variant_chunks_in_batch):
-            arr_rechunked[v_sel, ...] = arr[v_sel, ...]
-
-    with transaction(
-        vcz, backend_storage=backend_storage, rearrange=True, message="rechunk rename"
-    ) as store:
-        session = store.session
-        session.move(f"/{var}", f"/{var_delete}")
-        session.move(f"/{var_rechunked}", f"/{var}")
-
-    with transaction(
-        vcz, backend_storage=backend_storage, message="rechunk delete"
-    ) as store:
-        root = open_zarr(store, mode="r+", backend_storage=backend_storage)
-        del root[var_delete]
